81ccad8609aa5952031c52c68c8e171fd4c146e2
Praveen Mahato <praveenmahato147@gmail.com>
2021-02-23 23:37:40 +0530

    added chuck norris dependency and service

---
+++ src/main/java/com/example/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes quotes;
+
+    public JokeServiceImpl() {
+        this.quotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return quotes.getRandomQuote();
+    }
+}
